3bcd46e2deb78fdd8873397318724655ee959ccc
Ngine20 <84970357+Ngine20@users.noreply.github.com>
2023-04-20 15:24:15 +0200

    Add files via upload

---
+++ kartka.js
@@ -0,0 +1,45 @@
+// import kaboom lib
+import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
+
+// initialize kaboom context
+kaboom();
+
+// add a piece of text at position (120, 80)
+
+
+
+
+loadSprite("tlo","tło.png")
+loadSprite("mikolaj","Mikołaj Wazowski.png")
+loadSound("muzyka", "DzisiajwBetlejem.mp3")
+
+
+
+play("muzyka")
+
+add([
+    sprite("tlo")
+])
+
+
+const mikolaj =add([
+    pos(100,125),
+    scale(0.7),
+    sprite ("mikolaj")
+])
+
+
+let wprawo = true
+
+mikolaj.onUpdate (() => {
+
+    if (wprawo && mikolaj.pos.x < 350)
+        mikolaj.pos.x += 1
+
+    else if(!wprawo && mikolaj.pos.x > 50)
+        mikolaj.pos.x -=1
+    else wprawo ^= true
+
+}
+)
+
